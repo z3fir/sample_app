5641e6b996ca40a02fe394459041457de938a964
Kamil R <coldfusator@gmail.com>
2011-03-25 21:34:25 +0100

    zrobiony layout i routowanie

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+
+  match '/signup', :to => 'users#new'
   root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
